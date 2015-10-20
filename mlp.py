b75396a1e62987e6e6119173396648e00c692645
masaponto <masaponto@gmail.com>
2015-10-20 10:21:09 +0900

    [ADD] binary classification with iris is available

--- mlp.py
+++ mlp.py
@@ -2,14 +2,17 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-import random
-import matplotlib.pyplot as plt
-import os
+
+from sklearn import preprocessing
+from sklearn.base import BaseEstimator
+from sklearn.datasets import fetch_mldata
+from sklearn import cross_validation
+from sklearn.datasets import load_svmlight_file
 
 
-class MLP:
+class MLP(BaseEstimator):
 
-    def __init__(self, mid_num, out_num, epochs, r = 0.5, a = 1):
+    def __init__(self, mid_num, epochs, r=0.5, a=1):
         """mlp using sigmoid
         mid_num: 中間層ノード数
         out_num: 出力層ノード数
@@ -18,13 +21,12 @@ class MLP:
         a: シグモイド関数の定数
         """
         self.mid_num = mid_num
-        self.out_num = out_num
         self.epochs = epochs
         self.r = r
         self.a = a
 
     def sigmoid(self, x):
-        return 1 / (1 + np.exp(-self.a * x))
+        return 1 / (1 + np.exp(- self.a * x))
 
     def sigmoid_(self, x):
         """シグモイド関数微分"""
@@ -47,16 +49,26 @@ class MLP:
         i_v = np.atleast_2d(i_v)
         return w_vs - self.r * np.dot(e_v.T, i_v)
 
-    def add_bias(self, x_v):
-        """バイアス項追加"""
-        return np.append(x_v, 1)
+    def add_bias(self, x_vs):
+        """add bias to list
+
+        Args:
+        x_vs [[float]] Array: vec to add bias
+
+        Returns:
+        [float]: added vec
+
+        """
+
+        return np.c_[x_vs, np.ones(len(x_vs))]
 
     def fit(self, X, y):
         """学習"""
-        x_vs = X
-        d_vs = y
 
-        x_vs = list(map(self.add_bias, x_vs))
+        d_vs = y
+        self.out_num = max(y)
+
+        x_vs = self.add_bias(X)
         x_vd = len(x_vs[0])
 
         # 重み
@@ -85,60 +97,102 @@ class MLP:
                 # 出力層
                 self.wo_vs = self.w_update(self.wo_vs, eo_v, mid_v)
 
-    def predict(self, x_v):
-        x_v = self.add_bias(x_v)
-        mid_v = self.calc_out(self.wm_vs, x_v)
-        out_v = self.calc_out(self.wo_vs, mid_v)
-        return out_v
+    def __vtol(self, vec):
+        """tranceform vector (list) to label
 
+        Args:
+        v: int list, list to transform
 
-def plot(mlp, x_vs, y_vs, fname):
-    """plot function
-    """
-    plt.cla()
-    plt.clf()
-    plt.close()
+        Returns:
+        int : label of classify result
 
-    plt.ylim((-1,2))
-    plt.xlim((-1,2))
+        Exmples:
+        >>> e = ELM(10, 3)
+        >>> e.out_num = 3
+        >>> e._ELM__vtol([1, -1, -1])
+        1
+        >>> e._ELM__vtol([-1, 1, -1])
+        2
+        >>> e._ELM__vtol([-1, -1, 1])
+        3
+        >>> e._ELM__vtol([-1, -1, -1])
+        0
 
-    for i in range(len(x_vs)):
-        x_v = x_vs[i]
-        x = x_v[0]
-        y = x_v[1]
+        """
 
-        if y_vs[i] == 1:
-            plt.plot(x, y, "r.", label= "["+ str(x) + ", "+ str(y) + "] class 1")
+        if self.out_num == 1:
+            return round(vec[0], 0)
         else:
-            plt.plot(x, y, "k.", label= "["+ str(x) + ", "+ str(y) + "] class 2")
+            v = list(vec)
+            if len(v) == 1:
+                return vec[0]
+            elif (max(v) == -1):
+                return 0
+            return int(v.index(1)) + 1
 
-    for w_v in mlp.wm_vs:
-        xs = range(-5, 5)
-        ys = [-(w_v[0] / w_v[1]) * x - (w_v[2] / w_v[1])
-              for x in xs]
-        plt.plot(xs, ys)
 
-    path = os.path.join("graphs", fname)
+    def predict(self, x_vs):
+        x_vs = self.add_bias(x_vs)
+        mid_vs = [self.calc_out(self.wm_vs, x_v) for x_v in x_vs]
+        out_vs = [ 1 if 1 == self.__vtol( self.calc_out( self.wo_vs, mid_v) ) else -1 for mid_v in mid_vs]
+        return np.array(out_vs)
 
-    plt.legend(loc="best")
-    plt.savefig(path)
+
+    def one_predict(self, x_v):
+        x_v = np.append(x_v, 1)
+
+        print(x_v)
+
+        mid_v = self.calc_out(self.wm_vs, x_v)
+        out_v = self.calc_out( self.wo_vs, mid_v)
+        print(out_v)
+
+        #return 1 if round(out_v) == 1 else -1
+
+        return 1 if self.__vtol(out_v)  == 1 else -1
 
 
 if __name__ == "__main__":
 
-    # data
-    X_train = [[0, 0], [1, 0], [0, 1], [1, 1]]
-    y_train = [1, 0, 0, 1]
+    db_name = 'australian'
+    #db_name = 'iris'
 
-    mid_num = 2
-    out_num = 1
-    epochs = 10000
+    hid_nums = [10]
 
-    mlp = MLP(mid_num, out_num, epochs)
+    data_set = fetch_mldata(db_name)
+    data_set.data = preprocessing.scale(data_set.data)
+
+    X_train, X_test, y_train, y_test = cross_validation.train_test_split(
+        data_set.data, data_set.target, test_size=0.4, random_state=0)
+
+    mlp = MLP(10,100)
     mlp.fit(X_train, y_train)
 
-    plot(mlp, X_train, y_train, "graph.png")
+    #print(y_test[0])
 
-    result = list(map(mlp.predict,X_train))
-    for i in range(len(result)):
-        print(str(result[i]) + ":" + str(y_train[i]))
+    #print(mlp.one_predict(X_test[0]))
+    #print(y_test == mlp.predict(X_test))
+
+    print(sum(y_test == mlp.predict(X_test)) / len(y_test) )
+
+
+
+
+#    for db_name in db_names:
+#        print(db_name)
+#        # load iris data set
+#        data_set = fetch_mldata(db_name)
+#        data_set.data = preprocessing.scale(data_set.data)
+#
+#        print('MLP')
+#        for hid_num in hid_nums:
+#            print(str(hid_num), end=' ')
+#            mlp = MLP(10,100)
+#            ave = 0
+#
+#            for i in range(3):
+#                scores = cross_validation.cross_val_score(
+#                    mlp, data_set.data, data_set.target, cv=5, scoring='accuracy')
+#                ave += scores.mean()
+#            ave /= 3
+#            print("Accuracy: %0.2f " % (ave))
